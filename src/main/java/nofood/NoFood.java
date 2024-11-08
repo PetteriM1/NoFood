594783e7322202b0b7040552f8c03101f03c82b5
Petteri <26197131+PetteriM1@users.noreply.github.com>
2024-11-08 23:33:55 +0200

    Update

--- src/main/java/nofood/NoFood.java
+++ src/main/java/nofood/NoFood.java
@@ -1,30 +1,47 @@
 package nofood;
 
 import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerEatFoodEvent;
 import cn.nukkit.event.player.PlayerFoodLevelChangeEvent;
 import cn.nukkit.plugin.PluginBase;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class NoFood extends PluginBase implements Listener {
+
+    private boolean allowEat;
+    private Set<String> worlds;
 
     @Override
     public void onEnable() {
-        this.getServer().getPluginManager().registerEvents(this, this);
-        this.saveDefaultConfig();
+        saveDefaultConfig();
+        allowEat = getConfig().getBoolean("AllowEat");
+        worlds = new HashSet<>(getConfig().getStringList("Worlds"));
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOW)
     public void onFood(PlayerEatFoodEvent e) {
-        if (this.getConfig().getStringList("Worlds").contains(e.getPlayer().getLevel().getName())) {
-            e.setCancelled(true);
+        if (allowEat) {
+            return;
         }
+        if (!worlds.contains(e.getPlayer().getLevel().getName())) {
+            return;
+        }
+        e.setCancelled(true);
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOW)
     public void onFood(PlayerFoodLevelChangeEvent e) {
-        if (this.getConfig().getStringList("Worlds").contains(e.getPlayer().getLevel().getName())) {
-            e.setCancelled(true);
+        if (allowEat && e.getFoodLevel() >= e.getPlayer().getFoodData().getLevel() && e.getFoodSaturationLevel() >= e.getPlayer().getFoodData().getFoodSaturationLevel()) {
+            return;
         }
+        if (!worlds.contains(e.getPlayer().getLevel().getName())) {
+            return;
+        }
+        e.setCancelled(true);
     }
 }
